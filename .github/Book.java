4078c3c2aba98294b4316ea3789cddf9f16480bb
Elvis <elvisboateng557@gmail.com>
2024-12-09 13:51:53 -0800

    Update Book.java

--- .github/Book.java
+++ .github/Book.java
@@ -1,35 +1,7 @@
-public class Book{
-    private String title;
-    private String author;
-    public boolean isAvailable;
+package org.example;
 
-    //constructor to initialize the book details
-    public Book(String title, String author){
-        this.title = title;
-        this.author = author;
-        this.isAvailable = true; // Default: the book is available
-    }
 
-    //getter and setter methods
-    public String getTitle(){
-        return title;
-    }
-
-    public String getAuthor(){
-        return author;
-    }
-
-    public boolean isAvailable(){
-        return isAvailable;
-    }
-
-    public void setAvailable(boolean available){
-        isAvailable = available;
-    }
-
-    //toString() method for displaying book details
-    @Override
-    public String toString(){
-        return "Book [Title=" + title + ", Author=" + author + ", Available=" + isAvailable + "]";
-    }
+public record Book(String title, String author, int yearPublished) {
+    // Constructor with parameters in it
+    
 }
